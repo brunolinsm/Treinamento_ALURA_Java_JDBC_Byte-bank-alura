b2122630a586aa5a22e63a82559f9a4497556fb2
Bruno Lins Maia <bruno.maia@serpro.gov.br>
2025-04-04 17:11:26 -0300

    Preparando para salvar conta

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,17 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoDB {
-    public static void main(String... x) {
+public class ConnectionFactory {
+    public Connection recuperarConexao(){
         try {
-            Connection connection = DriverManager
+            return DriverManager
                     .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=TreinamentoAlur@1");
-
-            System.out.println("Recuperei a conexão");
-
-            connection.close();
         } catch (SQLException e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
     }
 }
