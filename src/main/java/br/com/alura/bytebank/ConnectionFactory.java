4e7b698f5680e7c108b1afbac7e08f1e9c5459b0
Bruno Lins Maia <bruno.maia@serpro.gov.br>
2025-04-22 17:27:56 -0300

    Pool de conexoes

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,10 +10,19 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection recuperarConexao(){
         try {
-            return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=TreinamentoAlur@1");
+            return createDataSource().getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("TreinamentoAlur@1");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
